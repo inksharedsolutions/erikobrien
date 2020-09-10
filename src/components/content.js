9b014261f382390dcc3b992b1ae259d461625225
inksharedsolutions <webdev.inkshared@gmail.com>
2020-09-10 09:35:42 +0800

    changes-9-10-2020

--- src/components/content.js
+++ src/components/content.js
@@ -38,7 +38,7 @@ const Content = (props) =>{
         }else if (slideState === 3){
             return ( <BookContentsTitle 
                 title={`The Last Admiral`}
-                spnTitle={`Part 3 of their Duareheim Staff Saga`}
+                spnTitle={`Part 3 of the Duareheim Staff Sag`}
                 content={`This prequel to Kur’s Rage is part 3 of The Duaredheim Staff 
                 Saga and portrays the final adventures of the 11th Fleet and 
                 their last admiral.`}
